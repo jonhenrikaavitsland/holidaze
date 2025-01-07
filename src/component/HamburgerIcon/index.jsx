0da61306d63075745e48debcaf8300263f55b0aa
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-01-07 13:07:57 +0100

    changed the size and padding of the menu icon

--- src/component/HamburgerIcon/index.jsx
+++ src/component/HamburgerIcon/index.jsx
@@ -1,8 +1,8 @@
 export default function HamburgerIcon() {
   return (
-    <div className="fixed p-2.5 top-0 right-0 lg:collapse">
-      <button className="p-2.5">
-        <img src="/bars-solid.svg" alt="menu" className="w-5 h-h-7.5" />
+    <div className="fixed p-3.5 top-0 right-0 lg:collapse">
+      <button className="p-1.5">
+        <img src="/bars-solid.svg" alt="menu" className="w-7.5 h-6.5" />
       </button>
     </div>
   );
