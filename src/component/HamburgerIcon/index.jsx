7dd65982cb8a106254f590318a927cd3dd655ba0
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-01-07 12:50:29 +0100

    hamburger menu component

---
+++ src/component/HamburgerIcon/index.jsx
@@ -0,0 +1,9 @@
+export default function HamburgerIcon() {
+  return (
+    <div className="fixed p-2.5 top-0 right-0 lg:collapse">
+      <button className="p-2.5">
+        <img src="/bars-solid.svg" alt="menu" className="w-5 h-h-7.5" />
+      </button>
+    </div>
+  );
+}
