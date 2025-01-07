4762d4e820b0bf5c5ccd57623cbe48a85ae705c2
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-01-07 11:49:22 +0100

    header logo component

--- src/component/Header/index.jsx
+++ src/component/Header/index.jsx
@@ -2,7 +2,7 @@ import Logo from "../Logo";
 
 export default function Header() {
   return (
-    <div className="flex justify-center p-5 bg-light-gray">
+    <div className="flex justify-center p-5 bg-light-gray lg:justify-start lg:p-10">
       <Logo />
     </div>
   );
--- src/component/Logo/index.jsx
+++ src/component/Logo/index.jsx
@@ -1,12 +1,14 @@
 export default function Logo() {
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center md:flex-row md:h-[75px] md:items-start">
       <div className="size-[3.125rem]">
         <img src="/public/logo_warm_200.png" alt="holidaze" />
       </div>
-      <span className="font-mono text-xl-leading-none text-deep-blue">
-        holidaze
-      </span>
+      <div className="md:h-full md:flex md:items-end">
+        <span className="font-mono text-xl-leading-none text-deep-blue md:text-5xl-50">
+          holidaze
+        </span>
+      </div>
     </div>
   );
 }
