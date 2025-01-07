11d8abf54d08b1620e26cc9e49b934f8a94b6f6d
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-01-07 12:52:55 +0100

    logo component done

--- src/component/Logo/index.jsx
+++ src/component/Logo/index.jsx
@@ -2,7 +2,7 @@ export default function Logo() {
   return (
     <div className="flex flex-col items-center md:flex-row md:h-[75px] md:items-start">
       <div className="size-[3.125rem]">
-        <img src="/public/logo_warm_200.png" alt="holidaze" />
+        <img src="/logo_warm_200.png" alt="holidaze" />
       </div>
       <div className="md:h-full md:flex md:items-end">
         <span className="font-mono text-xl-leading-none text-deep-blue md:text-5xl-50">
