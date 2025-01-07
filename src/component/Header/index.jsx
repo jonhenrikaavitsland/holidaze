f1b370e1c32b3aaf47697cc3fbf628208741d33b
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-01-07 12:57:55 +0100

    header component now returns a header element

--- src/component/Header/index.jsx
+++ src/component/Header/index.jsx
@@ -3,9 +3,9 @@ import Logo from "../Logo";
 
 export default function Header() {
   return (
-    <div className="flex justify-center p-5 bg-light-gray lg:justify-start lg:p-10 relative">
+    <header className="flex justify-center p-5 bg-light-gray lg:justify-start lg:p-10 relative">
       <Logo />
       <HamburgerIcon />
-    </div>
+    </header>
   );
 }
