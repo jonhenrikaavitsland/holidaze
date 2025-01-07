db28381085b1daf3d046975b3a11e2226343eb62
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-01-07 10:25:50 +0100

    extending fontFamily in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from "tailwindcss/defaultTheme";
+
 export default {
-  content: ['./**/index.html', './src/**/*.{js,ts,jsx,tsx,mjs,cjs}'],
+  content: ["./**/index.html", "./src/**/*.{js,ts,jsx,tsx,mjs,cjs}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Roboto", ...defaultTheme.fontFamily.sans],
+        serif: ["Poppins", ...defaultTheme.fontFamily.serif],
+        mono: ["Nosifer", ...defaultTheme.fontFamily.mono],
+      },
+    },
   },
   plugins: [],
 };
