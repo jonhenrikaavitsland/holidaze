a75e51e6f6e88d5eff0630dd283ae53e5eced672
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-01-08 12:58:10 +0100

    footer component

--- src/component/Footer/index.jsx
+++ src/component/Footer/index.jsx
@@ -1,1 +1,7 @@
-export default function Footer() {}
+export default function Footer() {
+  return (
+    <div className="flex justify-center bg-light-gray pt-16 pb-5 md:pb-7.5">
+      <span>©️ Holidaze 2025</span>
+    </div>
+  );
+}
