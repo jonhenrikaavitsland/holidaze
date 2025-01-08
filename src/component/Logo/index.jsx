6664203a166b61ddd1b06c5fb903afcb0fa5f0d4
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-01-08 12:44:10 +0100

    fixed an issue were the logo would not stay centered in tablet view

--- src/component/Logo/index.jsx
+++ src/component/Logo/index.jsx
@@ -1,14 +1,16 @@
 export default function Logo() {
   return (
-    <div className="inline-flex flex-col items-center md:flex-row md:h-[75px] md:items-start">
-      <div className="w-[3.125rem]">
-        <img src="/logo_warm_200.png" alt="holidaze" />
+    <figure className="flex justify-center">
+      <div className="flex flex-col items-center md:flex-row md:h-[75px] md:items-start w-min">
+        <div className="w-[3.125rem]">
+          <img src="/logo_warm_200.png" alt="holidaze" />
+        </div>
+        <div className="md:h-full md:flex md:items-end">
+          <span className="font-mono text-xl-leading-none text-deep-blue md:text-5xl-50">
+            holidaze
+          </span>
+        </div>
       </div>
-      <div className="md:h-full md:flex md:items-end">
-        <span className="font-mono text-xl-leading-none text-deep-blue md:text-5xl-50">
-          holidaze
-        </span>
-      </div>
-    </div>
+    </figure>
   );
 }
