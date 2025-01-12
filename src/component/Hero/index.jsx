eb59d037c949ba71f3a4404b830a8f01149a7948
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-01-12 22:50:52 +0100

    hero component

---
+++ src/component/Hero/index.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import hero1 from "/src/data/hero/hero_1.jpg";
+import hero2 from "/src/data/hero/hero_2.jpg";
+import hero3 from "/src/data/hero/hero_3.jpg";
+import hero4 from "/src/data/hero/hero_4.jpg";
+
+const images = [hero1, hero2, hero3, hero4];
+
+export default function Hero() {
+  const [currentImage, setCurrentImage] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImage((prev) => (prev + 1) % images.length);
+    }, 20000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <section
+      className="h-60 md:h-[480px] bg-cover bg-center transition-all duration-1000 pt-7.5 md:pt-23"
+      style={{ backgroundImage: `url(${images[currentImage]})` }}
+    >
+      <div className="bg-natural-charcoal/80 text-white font-serif font-black text-center text-2xl md:text-[2.5rem] p-4 md:p-7.5">
+        <h1>We know Fuerteventura!</h1>
+      </div>
+    </section>
+  );
+}
