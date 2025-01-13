49cbb07434d598493bd18a7127ba0fdb7ac53269
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-01-13 20:54:20 +0100

    rendering the cards

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,17 +1,26 @@
+import CardLocation from "../../component/CardLocation";
 import Hero from "../../component/Hero";
 import SearchBox from "../../component/SearchBox";
+import locationData from "../../data/locations/locations.json";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-10">
-      <div className="grid grid-rows-hero grid-cols-hero md:grid-cols-heroMd md:grid-rows-heroMd lg:grid-cols-heroLg relative">
-        <div className="row-start-1 row-end-3 col-span-full bg-natural-charcoal"></div>
-        <Hero />
-        <SearchBox />
+    <>
+      <div className="flex flex-col gap-10">
+        <div className="grid grid-rows-hero grid-cols-hero md:grid-cols-heroMd md:grid-rows-heroMd lg:grid-cols-heroLg relative">
+          <div className="row-start-1 row-end-3 col-span-full bg-natural-charcoal"></div>
+          <Hero />
+          <SearchBox />
+        </div>
+        <h2 className="font-serif font-bold text-center text-deep-blue text-xl-leading-none mb-5">
+          Explore Fuerteventura
+        </h2>
       </div>
-      <h2 className="font-serif font-bold text-center text-deep-blue text-xl-leading-none mb-5">
-        Explore Fuerteventura
-      </h2>
-    </div>
+      <div className="flex flex-col gap-5">
+        {locationData.map((location) => (
+          <CardLocation key={location.name} location={location} />
+        ))}
+      </div>
+    </>
   );
 }
