1515e8e3bac8d041bb83beaac5ada7490153a950
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-01-13 04:07:30 +0100

    the grid now also includes desktop screen

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,7 +4,7 @@ import SearchBox from "../../component/SearchBox";
 export default function Home() {
   return (
     <>
-      <div className="grid grid-rows-hero grid-cols-hero md:grid-cols-heroMd md:grid-rows-heroMd relative">
+      <div className="grid grid-rows-hero grid-cols-hero md:grid-cols-heroMd md:grid-rows-heroMd lg:grid-cols-heroLg relative">
         <Hero />
         <SearchBox />
       </div>
