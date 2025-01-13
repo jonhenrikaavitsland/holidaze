7b419252318097f3252fc9fecccff638f7ffb1aa
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-01-13 04:21:21 +0100

    div is now 1 colon grid if breakpoint is sm

--- src/component/SearchBox/index.jsx
+++ src/component/SearchBox/index.jsx
@@ -29,7 +29,7 @@ function SearchBar() {
 
 function Locations({ locationData }) {
   return (
-    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 md:gap-7.5">
+    <div className="grid sm:grid-cols-1 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 md:gap-7.5">
       <LocationBtn content="All Destinations" />
       {locationData.map((location, index) => (
         <LocationBtn key={index} content={location} />
