5235a11404b0f91bfc948ef1c0a370d5b96182ad
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-01-13 03:51:33 +0100

    adjusting grid for tablet size

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,7 +4,7 @@ import SearchBox from "../../component/SearchBox";
 export default function Home() {
   return (
     <>
-      <div className="grid grid-rows-hero grid-cols-hero relative">
+      <div className="grid grid-rows-hero grid-cols-hero md:grid-cols-heroMd md:grid-rows-heroMd relative">
         <Hero />
         <SearchBox />
       </div>
