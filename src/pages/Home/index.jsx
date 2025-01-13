ed8030defe2d7a78c9f0e03941708767953fa79f
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-01-13 17:36:41 +0100

    adds black bg behind the fading images

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <>
       <div className="grid grid-rows-hero grid-cols-hero md:grid-cols-heroMd md:grid-rows-heroMd lg:grid-cols-heroLg relative">
+        <div className="row-start-1 row-end-3 col-span-full bg-natural-charcoal"></div>
         <Hero />
         <SearchBox />
       </div>
