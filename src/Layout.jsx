15789126577c5986e1fcbc0f007df8fbe3066686
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-01-14 21:27:23 +0100

    margin bottom tablet

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -14,7 +14,7 @@ export default function Layout() {
     <div className="flex flex-col h-screen">
       <Header isOpen={isOpen} toggleMenu={toggleMenu} />
       <main
-        className={`flex-grow lg:container lg:mx-auto mb-10 lg:mb-20 ${isOpen ? "overflow-hidden" : ""}`}
+        className={`flex-grow lg:container lg:mx-auto mb-10 md:mb-15 lg:mb-20 ${isOpen ? "overflow-hidden" : ""}`}
       >
         <Outlet />
         <div
