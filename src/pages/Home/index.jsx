d4ae57b271988d0126bfac84db80361ed1a17c29
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-01-14 23:14:08 +0100

    preparing new state to store data that will be fetched from the api

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,16 +1,21 @@
+import { useState } from "react";
 import CardLocation from "../../component/CardLocation";
 import Hero from "../../component/Hero";
 import SearchBox from "../../component/SearchBox";
 import locationData from "../../data/locations/locations.json";
 
 export default function Home() {
+  const [fetchedData, setFetchedData] = useState([]);
+
+  console.log("Fetched Data:", fetchedData);
+
   return (
     <>
       <div className="flex flex-col gap-10 md:gap-12.5 lg:gap-15">
         <div className="grid grid-rows-hero grid-cols-hero md:grid-cols-heroMd md:grid-rows-heroMd lg:grid-cols-heroLg relative">
           <div className="row-start-1 row-end-3 col-span-full bg-natural-charcoal"></div>
           <Hero />
-          <SearchBox />
+          <SearchBox setFetchedData={setFetchedData} />
         </div>
         <h2 className="font-serif font-bold text-center text-deep-blue text-xl-leading-none md:text-2xl-leading-none lg:text-3xl-leading-none mb-5 md:mb-7.5">
           Explore Fuerteventura
