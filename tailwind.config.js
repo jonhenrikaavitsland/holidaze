43f5e51d283bf3b3075f3fee8adf1c465ce52f03
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-01-14 21:48:05 +0100

    custom gap sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -86,6 +86,8 @@ export default {
       },
       gap: {
         7.5: "30px",
+        12.5: "50px",
+        15: "60px",
       },
       boxShadow: {
         md: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)",
