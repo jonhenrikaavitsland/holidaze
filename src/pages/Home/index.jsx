d7068d0cb121446b10152bf5864264b40b2497e9
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-01-14 21:49:31 +0100

    text size desktop

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,13 +6,13 @@ import locationData from "../../data/locations/locations.json";
 export default function Home() {
   return (
     <>
-      <div className="flex flex-col gap-10">
+      <div className="flex flex-col gap-10 md:gap-12.5 lg:gap-15">
         <div className="grid grid-rows-hero grid-cols-hero md:grid-cols-heroMd md:grid-rows-heroMd lg:grid-cols-heroLg relative">
           <div className="row-start-1 row-end-3 col-span-full bg-natural-charcoal"></div>
           <Hero />
           <SearchBox />
         </div>
-        <h2 className="font-serif font-bold text-center text-deep-blue text-xl-leading-none mb-5">
+        <h2 className="font-serif font-bold text-center text-deep-blue text-xl-leading-none md:text-2xl-leading-none lg:text-3xl-leading-none mb-5 md:mb-7.5">
           Explore Fuerteventura
         </h2>
       </div>
