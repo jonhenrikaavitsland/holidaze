b085f074d083b74764101c68fc264407f5350601
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-01-15 22:50:04 +0100

    removed logs

--- src/component/CardVenue/index.jsx
+++ src/component/CardVenue/index.jsx
@@ -6,8 +6,6 @@ import NamePlate from "../NamePlate";
 export default function CardVenue({ venue, className }) {
   const [isHovered, setIsHovered] = useState(false);
 
-  console.log("Venue:", venue);
-
   return (
     <div
       className={`bg-cover bg-no-repeat bg-center lg:rounded-xl h-56 md:h-110 lg:h-96 cursor-pointer shadow-md shadow-natural-charcoal/40 ${className} hover:contrast-125 grid grid-rows-locationCard md:grid-rows-locationCardMd`}
