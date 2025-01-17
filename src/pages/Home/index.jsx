084aded11ee67c27e3f6235c88d24dc7f9c0e36e
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-01-17 05:56:56 +0100

    styling btn

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -102,7 +102,7 @@ function ViewMoreBtn({ data, paginateData }) {
   return (
     <button
       onClick={() => paginateData(data, 10)}
-      className="font-serif font-bold capitalize text-natural-charcoal bg-golden-yellow rounded-xl shadow-md shadow-natural-charcoal/40 hover:bg-golden-yellow/90 lg:mt-10 lg:py-5 lg:px-10"
+      className="font-serif font-bold text-xl-leading-none md:text-2xl-leading-none lg:text-3xl-leading-none capitalize text-natural-charcoal bg-golden-yellow rounded-xl shadow-md shadow-natural-charcoal/40 hover:bg-golden-yellow/90 mt-5 md:mt-7.5 lg:mt-10 py-3.75 px-7.5 md:py-5 md:px-10"
     >
       load more
     </button>
