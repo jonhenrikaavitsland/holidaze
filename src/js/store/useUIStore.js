10e9f0225e429d533abec7b106f2cc47f1490aee
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-01-18 00:22:27 +0100

    zustand store to handle opening and closing of UI components

---
+++ src/js/store/useUIStore.js
@@ -0,0 +1,44 @@
+import { create } from "zustand";
+
+const useUIStore = create((set) => ({
+  isOverlayOpen: false,
+  isMenuOpen: false,
+  isLoginModalOpen: false,
+  isRegisterModalOpen: false,
+
+  // Function to open a state and overlay together
+  openStateWithOverlay: (stateKey) =>
+    set(() => ({
+      isOverlayOpen: true,
+      [stateKey]: true,
+    })),
+
+  // Function to close everything
+  closeAll: () =>
+    set(() => ({
+      isOverlayOpen: false,
+      isMenuOpen: false,
+      isLoginModalOpen: false,
+      isRegisterModalOpen: false,
+    })),
+
+  // Utility to check if any state is open and close everything
+  checkAndCloseAll: () =>
+    set((state) => {
+      if (
+        state.isMenuOpen ||
+        state.isLoginModalOpen ||
+        state.isRegisterModalOpen
+      ) {
+        return {
+          isOverlayOpen: false,
+          isMenuOpen: false,
+          isLoginModalOpen: false,
+          isRegisterModalOpen: false,
+        };
+      }
+      return {}; // No state changes needed
+    }),
+}));
+
+export default useUIStore;
