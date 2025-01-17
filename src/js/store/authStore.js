0e9f7b5c182453d59fdf321ea852220528b59a2b
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-01-17 09:17:37 +0100

    auth api-hook

---
+++ src/js/store/authStore.js
@@ -0,0 +1,32 @@
+import { create } from "zustand";
+
+export function useAutStore() {
+  create((set) => ({
+    user: null,
+    token: null,
+    isLoggedIn: false,
+    login: (name, email, avatar, token) => {
+      // Update Zustand state
+      set({ user: { name, email, avatar }, token, isLoggedIn: true });
+      // Save to LocalStorage
+      localStorage.setItem(
+        "auth",
+        JSON.stringify({ user: { name, email, avatar }, token }),
+      );
+    },
+    logout: () => {
+      // Clear Zustand store
+      set({ user: null, token: null, isLoggedIn: false });
+      // Delete from LocalStorage
+      localStorage.removeItem("auth");
+    },
+    initializeAuth: () => {
+      // Check LocalStorage for saved auth data
+      const storedAuth = localStorage.getItem("auth");
+      if (storedAuth) {
+        const { user, token } = JSON.parse(storedAuth);
+        set({ user, token, isLoggedIn: true });
+      }
+    },
+  }));
+}
