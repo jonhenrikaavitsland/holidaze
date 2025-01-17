9995991705962ca8772a41e55e4bf3bb6f6efe7e
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-01-17 06:41:25 +0100

    expanding on the footer

--- src/component/Footer/index.jsx
+++ src/component/Footer/index.jsx
@@ -1,7 +1,35 @@
+import { Link } from "react-router-dom";
+
 export default function Footer() {
   return (
-    <footer className="flex justify-center bg-light-gray pt-16 pb-5 md:pb-7.5">
-      <span>©️ Holidaze 2025</span>
+    <footer className="flex flex-col items-center text-center  bg-light-gray pt-7.5 pb-5">
+      <ul className="flex flex-col gap-5 pb-5 capitalize font-serif">
+        <li>
+          <Link className="p-2.5" to="/">
+            home
+          </Link>
+        </li>
+        <li>
+          <button
+            className="p-2.5 capitalize"
+            onClick={
+              {
+                /* trigger login functionality */
+              }
+            }
+          >
+            log in
+          </button>
+        </li>
+        <li>
+          <Link className="p-2.5" to="/list-your-venue">
+            list your venue
+          </Link>
+        </li>
+      </ul>
+      <div className="border-t-2 border-natural-charcoal/20 w-dvw pt-5">
+        <span>© Holidaze 2025</span>
+      </div>
     </footer>
   );
 }
