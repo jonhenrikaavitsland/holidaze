5cd4e8d1acbbcfa34204caa21121580336509bdd
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-01-18 00:38:13 +0100

    added closing btn to the overlay

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,9 +2,11 @@ import { Outlet } from "react-router-dom";
 import Header from "./component/Header";
 import Footer from "./component/Footer";
 import { useCallback, useState } from "react";
+import useUIStore from "./js/store/useUIStore";
 
 export default function Layout() {
   const [isOpen, setIsOpen] = useState(false);
+  const { isOverlayOpen, closeAll } = useUIStore();
 
   const toggleMenu = useCallback(() => {
     setIsOpen((prev) => !prev);
@@ -19,15 +21,20 @@ export default function Layout() {
         <Outlet />
         <div
           className={
-            isOpen
-              ? "absolute top-0 left-0 w-dvw h-dvh bg-natural-charcoal/80 z-30 cursor-pointer"
+            isOverlayOpen
+              ? "absolute flex justify-end p-2.5 top-0 left-0 w-dvw h-dvh bg-natural-charcoal/80 z-30 cursor-pointer"
               : "collapse"
           }
-          onClick={toggleMenu}
+          onClick={closeAll}
         >
           <div>
-            <button>
-              <img src="/public/xmark-solid.svg" alt="close" />
+            <button className="p-2.5 hover:bg-white/20 rounded-xl">
+              <img
+                src="/xmark-solid-white.svg"
+                alt="close"
+                onClick={closeAll}
+                className="w-6.5 h-6.5"
+              />
             </button>
           </div>
         </div>
