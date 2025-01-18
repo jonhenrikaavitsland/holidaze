c27a91797fe542edd48df06f084423fb4957b1b6
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-01-18 03:48:49 +0100

    logo text color is given by props

--- src/component/Logo/index.jsx
+++ src/component/Logo/index.jsx
@@ -4,10 +4,12 @@ export default function Logo(props) {
     <figure className="flex justify-center">
       <div className="flex flex-col items-center md:flex-row md:h-[75px] md:items-start w-min">
         <div className="w-[3.125rem]">
-          <img src={`/logo_${props.color}_200.png`} alt="holidaze" />
+          <img src="/logo_warm_200.png" alt="holidaze" />
         </div>
         <div className="md:h-full md:flex md:items-end">
-          <span className="font-mono text-xl-leading-none text-deep-blue md:text-5xl-50">
+          <span
+            className={`font-mono text-xl-leading-none text-${props.color} md:text-5xl-50`}
+          >
             holidaze
           </span>
         </div>
