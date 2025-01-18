19b68db4cd7cfb0d05bab41cea09d27d5d52d0f0
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-01-18 03:51:02 +0100

    removed Label Component as it was not effective

--- src/component/Label/index.jsx
+++
@@ -1,8 +0,0 @@
-/* eslint-disable react/prop-types */
-export default function Label(props) {
-  return (
-    <label className={`${props.className}`} htmlFor={props.target}>
-      {props.content}
-    </label>
-  );
-}
