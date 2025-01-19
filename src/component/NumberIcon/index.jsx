2c90a69e163ab7baa4ef76d27b1313eb964fdda9
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-01-20 00:20:12 +0100

    fixing an issue with class not working properly

--- src/component/NumberIcon/index.jsx
+++ src/component/NumberIcon/index.jsx
@@ -1,11 +1,11 @@
 /* eslint-disable react/prop-types */
-export default function NumberIcon({ number, size }) {
+export default function NumberIcon(props) {
   return (
     <figure>
       <img
-        className={`bg-natural-charcoal rounded-full h-${size} w-${size}`}
-        src={`/circle-${number}.svg`}
-        alt={`number${number}`}
+        className={`bg-natural-charcoal rounded-full ${props.height}`}
+        src={`/circle-${props.number}.svg`}
+        alt={`number${props.number}`}
       />
     </figure>
   );
