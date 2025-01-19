c11a730ce5227f9ba96ab9fe509318bb6ecaab3b
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-01-19 19:57:11 +0100

    hoverstates footer

--- src/component/Footer/index.jsx
+++ src/component/Footer/index.jsx
@@ -8,21 +8,29 @@ export default function Footer() {
     <footer className="flex flex-col items-center text-center  bg-light-gray pt-7.5 pb-5">
       <ul className="flex flex-col gap-5 pb-5 capitalize font-serif">
         <li>
-          <Link className="p-2.5" to="/">
+          <Link className="p-2.5 hover:bg-golden-yellow/20 rounded-xl" to="/">
             home
           </Link>
         </li>
         <li>
           {isLoggedIn ? (
-            <Link className="p-2.5" to="/account/">
+            <Link
+              className="p-2.5 hover:bg-golden-yellow/20 rounded-xl"
+              to="/account/"
+            >
               Account
             </Link>
           ) : (
-            <button className="p-2.5 capitalize">log in</button>
+            <button className="p-2.5 capitalize hover:bg-golden-yellow/20 rounded-xl">
+              log in
+            </button>
           )}
         </li>
         <li>
-          <Link className="p-2.5" to="/list-your-venue/">
+          <Link
+            className="p-2.5 hover:bg-golden-yellow/20 rounded-xl"
+            to="/list-your-venue/"
+          >
             list your venue
           </Link>
         </li>
