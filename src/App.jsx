19db0fb60fbf08756e70fae25ebbe961acbd5366
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-01-19 23:01:07 +0100

    element added to route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "./Layout";
 import Home from "./pages/Home";
+import ListYourVenue from "./pages/ListYourVenue";
 
 export default function App() {
   return (
@@ -8,7 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="/list-your-venue/" />
+          <Route path="/list-your-venue/" element={<ListYourVenue />} />
         </Route>
       </Routes>
     </div>
