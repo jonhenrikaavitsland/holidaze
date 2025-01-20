9fc07e65600423bd3b0bdfae33c64dc00ad5d60b
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-01-20 07:04:37 +0100

    now confitionally rendering overlay

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -4,6 +4,7 @@ import Footer from "./component/Footer";
 import useUIStore from "./js/store/useUIStore";
 import LoginModal from "./component/LoginModal";
 import RegisterModal from "./component/RegisterModal";
+import Overlay from "./component/Overlay";
 
 export default function Layout() {
   const { isOverlayOpen, isLoginModalOpen, isRegisterModalOpen } = useUIStore();
@@ -15,37 +16,11 @@ export default function Layout() {
         className={`flex-grow lg:container lg:mx-auto mb-10 md:mb-15 lg:mb-20 ${isOverlayOpen ? "overflow-hidden" : ""}`}
       >
         <Outlet />
-        <Overlay />
         {isLoginModalOpen ? <LoginModal /> : ""}
         {isRegisterModalOpen ? <RegisterModal /> : ""}
+        {isOverlayOpen ? <Overlay /> : ""}
       </main>
       <Footer />
-    </div>
-  );
-}
-
-function Overlay() {
-  const { isOverlayOpen, closeAll } = useUIStore();
-
-  return (
-    <div
-      className={
-        isOverlayOpen
-          ? "absolute flex justify-end p-2.5 top-0 left-0 w-dvw h-dvh bg-natural-charcoal/80 z-40 cursor-pointer"
-          : "hidden"
-      }
-      onClick={closeAll}
-    >
-      <div>
-        <button className="p-2.5 hover:bg-white/20 rounded-xl">
-          <img
-            src="/xmark-solid-white.svg"
-            alt="close"
-            onClick={closeAll}
-            className="w-6.5 h-6.5"
-          />
-        </button>
-      </div>
     </div>
   );
 }
