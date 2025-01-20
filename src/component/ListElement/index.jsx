05074f083c78b0a9694296eca22d0624b93372b0
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-01-20 02:43:42 +0100

    tablet styling

--- src/component/ListElement/index.jsx
+++ src/component/ListElement/index.jsx
@@ -3,14 +3,14 @@ import NumberIcon from "../NumberIcon";
 /* eslint-disable react/prop-types */
 export default function ListElement(props) {
   return (
-    <li className="flex flex-col gap-2.5" key={props.index}>
-      <section className="flex gap-2.5">
+    <li className="flex flex-col gap-2.5 md:gap-3.75" key={props.index}>
+      <section className="flex gap-2.5 md:gap-5">
         <NumberIcon number={props.number} height={props.height} />
-        <h3 className="font-serif font-bold text-lg-leading-none">
+        <h3 className="font-serif font-bold text-lg-leading-none md:text-xl-leading-none">
           {props.item.title}
         </h3>
       </section>
-      <p>{props.item.text}</p>
+      <p className="text-lg">{props.item.text}</p>
     </li>
   );
 }
