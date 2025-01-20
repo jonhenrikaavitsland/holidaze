3f0f17fb5f6a7c7a8a69345abcc16fc9242b9c5b
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-01-20 07:04:18 +0100

    moved overlay to its own file

---
+++ src/component/Overlay/index.jsx
@@ -0,0 +1,25 @@
+import useUIStore from "../../js/store/useUIStore";
+
+export default function Overlay() {
+  const { closeAll } = useUIStore();
+
+  return (
+    <div
+      className="absolute flex justify-end p-2.5 top-0 left-0 w-dvw h-dvh bg-natural-charcoal/80 z-40 cursor-pointer"
+      onClick={closeAll}
+    >
+      <div className="lg:container lg:mx-auto flex w-full justify-center md:justify-end ">
+        <div>
+          <button className="p-2.5 hover:bg-white/20 rounded-xl">
+            <img
+              src="/xmark-solid-white.svg"
+              alt="close"
+              onClick={closeAll}
+              className="w-6.5 h-6.5"
+            />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
