50d3b44664385ce105e0217a999f38ea19b5a57a
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-01-20 08:38:18 +0100

    conditionally rendering route and adding a 404 route

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./Layout";
 import Home from "./pages/Home";
 import ListYourVenue from "./pages/ListYourVenue";
+import useAuthStore from "./js/store/useAuthStore";
 
 export default function App() {
+  const { isVenueManager } = useAuthStore();
+
   return (
     <div>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="/list-your-venue/" element={<ListYourVenue />} />
+          {!isVenueManager ? (
+            <Route path="/list-your-venue/" element={<ListYourVenue />} />
+          ) : (
+            <Route
+              path="/list-your-venue/"
+              element={<Navigate to="/404" replace />}
+            />
+          )}
+          {/* Catch-all 404 route */}
+          {/* <Route path='*' element={<NotFound />} /> */}
         </Route>
       </Routes>
     </div>
