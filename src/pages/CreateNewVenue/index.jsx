10843735a034300c847673d0deb51ff07848b212
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-01-21 01:53:25 +0100

    heading h1

--- src/pages/CreateNewVenue/index.jsx
+++ src/pages/CreateNewVenue/index.jsx
@@ -1,5 +1,14 @@
 import BreadCrumb from "../../component/Breadcrumb";
 
 export default function CreateNewVenue() {
-  return <div>{<BreadCrumb />}</div>;
+  return (
+    <div>
+      {<BreadCrumb />}
+      <section className="pt-5 px-5 pb-10">
+        <h1 className="font-bold font-serif text-deep-blue text-xl-leading-none text-center">
+          Create new Venue
+        </h1>
+      </section>
+    </div>
+  );
 }
