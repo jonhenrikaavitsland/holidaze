fc20745f5d8c024e877374f22b5cbec8c7ad50b6
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-01-21 10:12:23 +0100

    added register url path to constants

--- src/js/data/constants.js
+++ src/js/data/constants.js
@@ -2,3 +2,4 @@ export const apiKey = import.meta.env.VITE_API_KEY;
 export const apiUrl = import.meta.env.VITE_API_URL;
 
 export const loginPath = "/auth/login";
+export const registerPath = "/auth/register";
