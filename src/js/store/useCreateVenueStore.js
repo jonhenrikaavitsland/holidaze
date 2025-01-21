20208e6ee2e800e3f0281db6cb20366a4b9971fe
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-01-22 00:53:22 +0100

    new states and functions to the store

--- src/js/store/useCreateVenueStore.js
+++ src/js/store/useCreateVenueStore.js
@@ -4,19 +4,16 @@ import { persist } from "zustand/middleware";
 const useCreateVenueStore = create(
   persist(
     (set) => ({
-      // Initial states for amenities
       wifi: false,
       breakfast: false,
       parking: false,
       pets: false,
 
-      // Toggle functions for amenities
       toggleWifi: () => set((state) => ({ wifi: !state.wifi })),
       toggleBreakfast: () => set((state) => ({ breakfast: !state.breakfast })),
       toggleParking: () => set((state) => ({ parking: !state.parking })),
       togglePets: () => set((state) => ({ pets: !state.pets })),
 
-      // Additional states
       venue: "",
       address: "",
       location: "",
@@ -25,7 +22,6 @@ const useCreateVenueStore = create(
       rating: 1,
       sleeps: "",
 
-      // Setters for form inputs
       setVenue: (value) => set(() => ({ venue: value })),
       setAddress: (value) => set(() => ({ address: value })),
       setLocation: (value) => set(() => ({ location: value })),
@@ -33,6 +29,48 @@ const useCreateVenueStore = create(
       setPrice: (value) => set(() => ({ price: value })),
       setRating: (value) => set(() => ({ rating: value })),
       setSleeps: (value) => set(() => ({ sleeps: value })),
+
+      "media-0": "",
+      "media-1": "",
+      "media-2": "",
+      "media-3": "",
+      "media-4": "",
+      "media-5": "",
+      "media-6": "",
+      "media-7": "",
+      "media-8": "",
+      "media-9": "",
+
+      setMedia: (index, value) => set(() => ({ [`media-${index}`]: value })),
+
+      description: "",
+      setDescription: (value) => set(() => ({ description: value })),
+
+      clearAll: () =>
+        set(() => ({
+          wifi: false,
+          breakfast: false,
+          parking: false,
+          pets: false,
+          venue: "",
+          address: "",
+          location: "",
+          zipCode: "",
+          price: "",
+          rating: 1,
+          sleeps: "",
+          "media-0": "",
+          "media-1": "",
+          "media-2": "",
+          "media-3": "",
+          "media-4": "",
+          "media-5": "",
+          "media-6": "",
+          "media-7": "",
+          "media-8": "",
+          "media-9": "",
+          description: "",
+        })),
     }),
     {
       name: "createVenue",
