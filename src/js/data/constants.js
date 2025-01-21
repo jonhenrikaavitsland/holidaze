bfd51afc46b77632d98e84d6e43baf509fc4a918
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-01-21 10:17:32 +0100

    added path to profiles

--- src/js/data/constants.js
+++ src/js/data/constants.js
@@ -3,3 +3,4 @@ export const apiUrl = import.meta.env.VITE_API_URL;
 
 export const loginPath = "/auth/login";
 export const registerPath = "/auth/register";
+export const profilesPath = "/holidaze/profiles";
