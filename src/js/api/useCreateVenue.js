5a1829d07665a1ae2e3b2f82e1f313ac049b6252
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-01-22 07:46:34 +0100

    now getting locationsmap from constants file

--- src/js/api/useCreateVenue.js
+++ src/js/api/useCreateVenue.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import useAuthStore from "../store/useAuthStore";
 import useCreateVenueStore from "../store/useCreateVenueStore";
+import { locationsMap } from "../data/constants";
 
 const useCreateVenue = (apiUrl, apiKey) => {
   const { token } = useAuthStore();
@@ -39,15 +40,6 @@ const useCreateVenue = (apiUrl, apiKey) => {
         media8,
         media9,
       } = values;
-
-      // Map location
-      const locationsMap = {
-        1: { city: "Corralejo", lat: 28.7296937, lng: -13.8671499 },
-        2: { city: "Costa Calma", lat: 28.1585283, lng: -14.2294205 },
-        3: { city: "Caleta de Fuste", lat: 28.500821, lng: -13.8628367 },
-        4: { city: "Morro Jable", lat: 28.0511096, lng: -14.351552 },
-        5: { city: "El Cotillo", lat: 28.6855572, lng: -14.0108625 },
-      };
 
       const selectedLocation = locationsMap[location] || {};
       const city = selectedLocation.city || null;
--- src/js/data/constants.js
+++ src/js/data/constants.js
@@ -4,3 +4,11 @@ export const apiUrl = import.meta.env.VITE_API_URL;
 export const loginPath = "/auth/login";
 export const registerPath = "/auth/register";
 export const profilesPath = "/holidaze/profiles";
+
+export const locationsMap = {
+  1: { city: "Corralejo", lat: 28.7296937, lng: -13.8671499 },
+  2: { city: "Costa Calma", lat: 28.1585283, lng: -14.2294205 },
+  3: { city: "Caleta de Fuste", lat: 28.500821, lng: -13.8628367 },
+  4: { city: "Morro Jable", lat: 28.0511096, lng: -14.351552 },
+  5: { city: "El Cotillo", lat: 28.6855572, lng: -14.0108625 },
+};
