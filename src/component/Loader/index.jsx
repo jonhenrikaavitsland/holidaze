e1f9292acc6d29b6176fb13f385c19b679c5019c
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-01-22 09:05:11 +0100

    loader component now using tailwind instead of plain css

--- src/component/Loader/index.jsx
+++ src/component/Loader/index.jsx
@@ -1,10 +1,13 @@
 export default function Loader() {
+  /* Source https://loading.io/css/
+  //  CC0 License which means we can use it freely */
   return (
-    <div className="lds-ellipsis">
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+    <div className="text-deep-blue box-border inline-block relative w-20 h-20">
+      <div className="loader left-2 animate-ldsEllipsis1"></div>
+      <div className="loader left-2 animate-ldsEllipsis2"></div>
+      <div className="loader left-8 animate-ldsEllipsis2"></div>
+      <div className="loader left-14 animate-ldsEllipsis3"></div>
     </div>
   );
 }
+// loader is a custom class.
