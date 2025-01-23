8af79753d70119b8297d9869cef048336db0d286
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-01-23 11:32:21 +0100

    added map component to the page

--- src/pages/VenuePage/index.jsx
+++ src/pages/VenuePage/index.jsx
@@ -1,6 +1,7 @@
+/* eslint-disable react/prop-types */
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { apiUrl, venuesPath } from "../../js/data/constants";
+import { apiUrl, locationsMap, venuesPath } from "../../js/data/constants";
 import Loader from "../../component/Loader";
 import Carousel from "../../component/Carousel";
 import BreadCrumb from "../../component/Breadcrumb";
@@ -49,12 +50,107 @@ export default function VenuePage() {
   console.log(data);
 
   return (
-    <>
-      <BreadCrumb venueId={venueId} venueName={data.name} />
-      <Carousel media={data.media} />
-      <section>
-        <h1>{data.name}</h1>
+    <div className="flex flex-col gap-10">
+      <div className="flex flex-col gap-5">
+        <BreadCrumb venueId={venueId} venueName={data.name} />
+        <Carousel media={data.media} />
+      </div>
+      <section className="flex flex-col gap-5">
+        <h1 className="text-center font-serif font-bold text-xl-leading-none text-deep-blue">
+          {data.name}
+        </h1>
+        <div className="flex sm:flex-col gap-5 ms-5 sm:mx-5">
+          <Includes data={data} />
+          <BtnCheckAvailability data={data} />
+        </div>
+        <p className="mx-5">{data.description}</p>
       </section>
+      <div className="flex flex-col gap-5 mx-5">
+        <Map data={data} />
+        <div className="flex flex-col leading-none gap-1">
+          <span>{data.name}</span>
+          <span>{data.location.address}</span>
+          <span>
+            {data.location.zip} {data.location.city}
+          </span>
+          <span>Fuerteventura, {data.location.country}</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Includes({ data }) {
+  const included = {
+    rating: `Rating of ${data.rating}`,
+    sleeps: `Sleeps up to ${data.sleeps}`,
+    wifi: data.meta.wifi ? "WiFi is available" : null,
+    breakfast: data.meta.breakfast ? "Breakfast is included" : null,
+    pets: data.meta.pets ? "Pets are welcome" : null,
+    parking: data.meta.parking ? "Parking is available" : null,
+  };
+
+  return (
+    <div className="bg-light-sky-blue rounded-xl pt-2.5 px-2.5 pb-5 flex flex-col gap-2.5 shadow-md shadow-natural-charcoal/40 grow">
+      {Object.entries(included).map(([key, value]) => {
+        if (value) {
+          return (
+            <div key={key} className="flex gap-2.5 leading-none">
+              <img src="/logo_warm_200.png" alt="icon" className="h-4" />
+              <span>{value}</span>
+            </div>
+          );
+        }
+        return null;
+      })}
+    </div>
+  );
+}
+
+function BtnCheckAvailability({ data }) {
+  return (
+    <button className="shadow-md shadow-natural-charcoal/40">
+      <div className="font-serif text-center bg-natural-charcoal text-white py-4 font-bold uppercase text-lg-leading-none w-48 sm:w-full">
+        <h2>{data.location.city}</h2>
+      </div>
+      <div className="text-center text-lg-leading-none font-bold py-2.5">
+        <span>{`€${data.price} / Night`}</span>
+      </div>
+      <div className="font-serif text-center bg-deep-blue text-white text-xl-leading-none font-bold capitalize py-3.75">
+        <p className="w-30 mx-auto">check availability</p>
+      </div>
+    </button>
+  );
+}
+
+function Map({ data }) {
+  const [isLoading, setIsLoading] = useState(true);
+  const location = Object.values(locationsMap).find(
+    (loc) => loc.lat === data.location.lat && loc.lng === data.location.lng,
+  );
+
+  // Fallback URL if no match is found
+  const mapUrl = location
+    ? location.map
+    : "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d763938.2269933398!2d-14.789922060442619!3d28.399025647562258!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xc47a473afc20f81%3A0x2ac71c13b5b57f23!2sFuerteventura!5e1!3m2!1sno!2sno!4v1737627755427!5m2!1sno!2sno"; // Default fallback map URL
+
+  return (
+    <>
+      {isLoading && (
+        <div className="mx-auto">
+          <Loader />
+        </div>
+      )}
+      <iframe
+        className="border-0 w-full"
+        src={mapUrl}
+        width="400"
+        height="300"
+        allowfullscreen=""
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+        onLoad={() => setIsLoading(false)}
+      ></iframe>
     </>
   );
 }
