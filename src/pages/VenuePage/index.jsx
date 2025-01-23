7536f95532a643cb3cea5a6542f4b6ac31d524d2
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-01-23 09:46:03 +0100

    added breadcrumb, carousel

--- src/pages/VenuePage/index.jsx
+++ src/pages/VenuePage/index.jsx
@@ -1,1 +1,60 @@
-export default function VenuePage() {}
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { apiUrl, venuesPath } from "../../js/data/constants";
+import Loader from "../../component/Loader";
+import Carousel from "../../component/Carousel";
+import BreadCrumb from "../../component/Breadcrumb";
+
+export default function VenuePage() {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  let { venueId } = useParams();
+
+  useEffect(() => {
+    async function getData(url) {
+      try {
+        setIsLoading(true);
+        setIsError(false);
+
+        const response = await fetch(url);
+        const json = await response.json();
+
+        setData(json.data);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    getData(`${apiUrl}${venuesPath}/${venueId}`);
+  }, [venueId]);
+
+  if (isLoading || !data) {
+    return (
+      <div className="flex justify-center">
+        <Loader />
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <div className="flex justify-center text-custom-coral font-bold">
+        Error
+      </div>
+    );
+  }
+
+  console.log(data);
+
+  return (
+    <>
+      <BreadCrumb venueId={venueId} venueName={data.name} />
+      <Carousel media={data.media} />
+      <section>
+        <h1>{data.name}</h1>
+      </section>
+    </>
+  );
+}
