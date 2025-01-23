b6ece3476882f6753c8e3fd5938deb1835f01d64
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-01-23 11:48:37 +0100

    map moved to its own file

---
+++ src/component/Map/index.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable react/prop-types */
+import { fallBackMap, locationsMap } from "../../js/data/constants";
+
+export default function Map({ data }) {
+  const location = Object.values(locationsMap).find(
+    (loc) => loc.lat === data.location.lat && loc.lng === data.location.lng,
+  );
+
+  // Fallback URL if no match is found
+  const mapUrl = location ? location.map : fallBackMap; // Default fallback map URL
+
+  return (
+    <iframe
+      className="border-0 w-full"
+      src={mapUrl}
+      width="400"
+      height="300"
+      allowFullScreen=""
+      loading="lazy"
+      referrerPolicy="no-referrer-when-downgrade"
+    ></iframe>
+  );
+}
