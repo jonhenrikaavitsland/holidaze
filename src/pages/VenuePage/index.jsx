d2f71e14a69332f96fd685bfab2718ea8fde3ec3
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-01-24 05:37:07 +0100

    address component styled for tablet

--- src/pages/VenuePage/index.jsx
+++ src/pages/VenuePage/index.jsx
@@ -138,13 +138,13 @@ function BtnCheckAvailability({ data }) {
 
 function Address({ data }) {
   return (
-    <div className="flex flex-col leading-none gap-1">
-      <span>{data.name}</span>
-      <span>{data.location.address}</span>
-      <span>
+    <div className="flex flex-col leading-none md:text-lg-leading-none gap-1 md:gap-2 justify-end">
+      <span className="w-max">{data.name}</span>
+      <span className="w-max">{data.location.address}</span>
+      <span className="w-max">
         {data.location.zip} {data.location.city}
       </span>
-      <span>Fuerteventura, {data.location.country}</span>
+      <span className="w-max">Fuerteventura, {data.location.country}</span>
     </div>
   );
 }
