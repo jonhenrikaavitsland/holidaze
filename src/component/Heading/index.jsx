a361754d8404076213d43332cb2f45b27f47932f
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-01-29 20:20:55 +0100

    heading component now returns correct styles based off input

--- src/component/Heading/index.jsx
+++ src/component/Heading/index.jsx
@@ -1,6 +1,25 @@
 /* eslint-disable react/prop-types */
-export default function Heading({ level, children }) {
+export default function Heading({ level, className, children }) {
   const Tag = `h${level}`;
 
-  return <Tag>{children}</Tag>;
+  let classes;
+
+  if (Tag === "h1") {
+    classes =
+      "text-2xl-leading-none md:text-3xl-leading-none lg:text-4xl-leading-none";
+  }
+  if (Tag === "h2") {
+    classes =
+      "text-xl-leading-none md:text-2xl-leading-none lg:text-3xl-leading-none";
+  }
+  if (Tag === "h3") {
+    classes =
+      "text-lg-leading-none md:text-xl-leading-none lg:text-2xl-leading-none";
+  }
+
+  return (
+    <Tag className={`font-serif font-bold ${classes} ${className}`}>
+      {children}
+    </Tag>
+  );
 }
