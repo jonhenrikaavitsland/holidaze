f1609a4f2ad5a267217b0fed585c7bc60839aea0
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-01-30 14:34:41 +0100

    arrival/da\eparture dates

--- src/pages/BookingPage/index.jsx
+++ src/pages/BookingPage/index.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 import { apiUrl, venuesPath } from "../../js/data/constants";
 import Loader from "../../component/Loader";
 import BreadCrumb from "../../component/Breadcrumb";
@@ -10,6 +10,10 @@ export default function BookingPage() {
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
   let { venueId } = useParams();
+  let [searchParams] = useSearchParams();
+
+  const fromDate = searchParams.get("from");
+  const toDate = searchParams.get("to");
 
   useEffect(() => {
     async function getData(url) {
@@ -55,6 +59,38 @@ export default function BookingPage() {
         <Heading level="1" className={"text-center text-deep-blue"}>
           let&apos;s book that holiday
         </Heading>
+      </div>
+      <div className="rounded-xl bg-light-sky-blue shadow-md shadow-natural-charcoal/40 mx-5 md:mx-7.5 lg:mx-10">
+        <div className="flex sm:flex-col">
+          <div className="max-w-1/2 sm:max-w-full">
+            <img
+              src={data.media[0].url}
+              alt={data.media[0].alt}
+              className="rounded-tl-xl sm:rounded-t-xl h-full object-cover"
+            />
+          </div>
+          <section className="px-5 flex flex-col items-center gap-2 py-5 w-full">
+            <h2 className="w-max uppercase text-deep-blue font-serif font-bold text-xl-leading-none">
+              {data.name}
+            </h2>
+            <span className="font-serif uppercase text-lg-leading-none font-bold">
+              {data.location.city}
+            </span>
+          </section>
+        </div>
+        <section className="pt-5 px-2.5">
+          <h3 className="font-serif font-bold">Booking Details:</h3>
+          <div className="bg-white w-full border border-natural-charcoal/40 pt-1 px-1 pb-2.5 flex flex-col gap-1">
+            <p className="uppercase text-xs-leading-none font-bold">
+              Arrival - departure
+            </p>
+            <div className="w-full flex justify-center">
+              <span className="font-bold leading-none">
+                {fromDate} - {toDate}
+              </span>
+            </div>
+          </div>
+        </section>
       </div>
     </div>
   );
