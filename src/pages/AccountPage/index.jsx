0ed6087342d757021515093c7e1d53d8d57441f4
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-02-02 19:07:11 +0100

    creating a bi card component and adding image to it

--- src/pages/AccountPage/index.jsx
+++ src/pages/AccountPage/index.jsx
@@ -1,3 +1,27 @@
+/* eslint-disable react/prop-types */
+import Heading from "../../component/Heading";
+import useAuthStore from "../../js/store/useAuthStore";
+
 export default function AccountPage() {
-  return <div></div>;
+  const { user } = useAuthStore();
+  return (
+    <section className="flex flex-col gap-5 md:gap-7.5 lg:gap-10 px-5 md:px-7.5 lg:px-10 pt-5 md:pt-7.5 lg:pt-10 pb-10 md:pb-15 lg:pb-20">
+      <Heading level="1" className="text-center text-deep-blue">
+        Account
+      </Heading>
+      <BioCard user={user} />
+    </section>
+  );
+}
+
+function BioCard({ user }) {
+  return (
+    <div className="relative flex flex-col pt-2.5 border border-natural-charcoal/40 rounded-xl">
+      <div
+        className="h-50 w-50 rounded-full shadow-md shadow-natural-charcoal/40 bg-cover bg-center"
+        style={{ backgroundImage: `url(${user.avatar.url})` }}
+        aria-label={user.avatar.alt} // Optional: for accessibility
+      />
+    </div>
+  );
 }
