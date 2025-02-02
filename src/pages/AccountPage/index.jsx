b3601ddeb850bbdd95aa463f13bbd2c4bccbdf0a
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-02-02 19:32:30 +0100

    bioCard name and email

--- src/pages/AccountPage/index.jsx
+++ src/pages/AccountPage/index.jsx
@@ -18,7 +18,7 @@ export default function AccountPage() {
 
 function BioCard({ user }) {
   return (
-    <div className="relative flex flex-col pt-2.5 md:pt-5 lg:pt-7.5 border border-natural-charcoal/40 rounded-xl">
+    <div className="relative flex flex-col gap-5 lg:gap-7.5 pt-2.5 md:pt-5 lg:pt-7.5 pb-10 lg:pb-15 border border-natural-charcoal/40 rounded-xl">
       <div
         className="w-1/2 max-h-50 max-w-50 aspect-square rounded-full shadow-md shadow-natural-charcoal/40 bg-cover bg-no-repeat bg-center mx-auto"
         style={{ backgroundImage: `url(${user.avatar.url})` }}
@@ -28,6 +28,14 @@ function BioCard({ user }) {
         <button className="p-2.5 rounded-xl">
           <img src="/pen-solid.svg" alt="update image" className="h-5" />
         </button>
+      </div>
+      <div className="flex flex-col gap-2.5 lg:gap-3.75 text-center font-medium">
+        <span className="capitalize text-xl-leading-none lg:text-2xl-leading-none">
+          {user.name}
+        </span>
+        <span className="leading-none lg:text-xl-leading-none break-words">
+          {user.email}
+        </span>
       </div>
     </div>
   );
