737addfaf09add1d1d073049320b1196d6e2461e
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-02-05 04:52:23 +0100

    added stylings for tablet

--- src/pages/VenueHubPage/index.jsx
+++ src/pages/VenueHubPage/index.jsx
@@ -200,7 +200,7 @@ function BookingCard({ venueBooking, index, maxNum }) {
         {index + 1} / {maxNum}
       </span>
       <div
-        className={`relative bg-light-sky-blue pt-2.5 px-2.5 rounded-xl shadow-md shadow-natural-charcoal/40 ${openState ? "pb-15 md:pb-20 lg:pb-24" : "cursor-pointer pb-10 md:pb-15"}`}
+        className={`relative bg-light-sky-blue pt-2.5 px-2.5 md:pt-5 md:px-5 rounded-xl shadow-md shadow-natural-charcoal/40 ${openState ? "pb-15 md:pb-20 lg:pb-24" : "cursor-pointer pb-10 md:pb-15"}`}
         onClick={() => setOpenState(!openState)}
       >
         {openState ? (
@@ -242,21 +242,21 @@ function BookingOpen({
 
   return (
     <div className="flex flex-col gap-5 md:gap-7.5 lg:gap-10">
-      <section className="flex flex-col gap-2.5">
+      <section className="flex flex-col gap-2.5 md:gap-5">
         <Heading level="3" className="text-deep-blue">
           booking details
         </Heading>
         <div className="flex flex-col gap-1 leading-none">
-          <span className="capitalized italic text-sm-leading-none">
+          <span className="capitalized italic text-sm-leading-none md:text-base md:leading-none">
             created{" "}
             <time dateTime={venueBooking.booking.created}>
               {formattedDateCreated}
             </time>
           </span>
-          <p className="font-medium text-xl-leading-none">
+          <p className="font-medium text-lg-leading-none md:text-xl-leading-none">
             {venueBooking.venue.name}
           </p>
-          <span>
+          <span className="md:text-lg-leading-none">
             <time dateTime={venueBooking.booking.dateFrom}>
               {formattedDateFrom}
             </time>{" "}
@@ -265,19 +265,21 @@ function BookingOpen({
               {formattedDateTo}
             </time>
           </span>
-          <p className="capitalize">
+          <p className="capitalize md:text-lg-leading-none">
             <span>{venueBooking.venue.location.city}</span>, fuerteventura
           </p>
-          <p>{venueBooking.venue.location.country}</p>
+          <p className="md:text-lg-leading-none">
+            {venueBooking.venue.location.country}
+          </p>
         </div>
         <div className="flex flex-col gap-1 mt-2.5">
-          <div className="flex gap-2.5 text-lg-leading-none font-medium">
+          <div className="flex gap-2.5 md:gap-5 text-lg-leading-none font-medium md:text-xl-leading-none">
             <p>Booking value:</p>
             <div className="bg-white px-1 grow">
               <span>€{dayCount * venueBooking.venue.price}</span>
             </div>
           </div>
-          <div className="flex gap-2.5 leading-none">
+          <div className="flex gap-2.5 md:gap-5 leading-none md:text-lg-leading-none">
             <p>Price per night:</p>
             <div className="bg-white px-1 grow">
               <span>€{venueBooking.venue.price}</span>
@@ -285,18 +287,18 @@ function BookingOpen({
           </div>
         </div>
       </section>
-      <section className="flex flex-col gap-2.5">
+      <section className="flex flex-col gap-2.5 md:gap-5">
         <Heading level="3" className="text-deep-blue">
           guest details
         </Heading>
-        <div className="flex flex-col gap-1 leading-none">
-          <div className="flex gap-2.5 ">
+        <div className="flex flex-col gap-1 leading-none md:text-lg-leading-none">
+          <div className="flex gap-2.5 md:gap-5">
             <p>Number of guests:</p>
             <div className="bg-white px-1 grow">
               <span>{venueBooking.booking.guests}</span>
             </div>
           </div>
-          <div className="flex gap-2.5 ">
+          <div className="flex gap-2.5 md:gap-5">
             <p>Number of guests:</p>
             <div className="bg-white px-1 grow">
               <span className="capitalize">
@@ -304,7 +306,7 @@ function BookingOpen({
               </span>
             </div>
           </div>
-          <div className="flex gap-2.5 ">
+          <div className="flex gap-2.5 md:gap-5">
             <p className="whitespace-nowrap">Contact email:</p>
             <div className="bg-white px-1 grow min-w-0">
               <span className="break-words">
