6a7c6f53062bd678ac5ae1da8d29848ec0b261b6
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-02-05 02:17:48 +0100

    moved comp to its own folder

--- src/pages/VenueHubPage/index.jsx
+++ src/pages/VenueHubPage/index.jsx
@@ -8,6 +8,7 @@ import LinkBtn from "./LinkBtn";
 import Buttons from "./Buttons";
 import Paragraph from "./Paragraph";
 import CreateNewBtn from "./CreateNewBtn";
+import Welcome from "./Welcome";
 
 /* eslint-disable react/prop-types */
 export default function VenueHubPage() {
@@ -76,6 +77,22 @@ export default function VenueHubPage() {
     </div>
   );
 }
+
+// function ViewBookings() {
+//   const [currentPage, setCurrentPage] = useState(1);
+
+//   const { venues, meta, loading, error } = useProfileVenues({ page: currentPage, limit: 100 });
+
+//   console.log('VENUES:', venues);
+
+//   return (
+//     <section>
+//       <Heading level='2' className='text-center text-custom-coral'>
+//         active bookings
+//       </Heading>
+//     </section>
+//   );
+// }
 
 function ViewVenuesObject({ handleViewChange, setCurrentVenue }) {
   const [currentPage, setCurrentPage] = useState(1);
@@ -322,59 +339,43 @@ function HasNoVenues({ handleViewChange }) {
   );
 }
 
-// function ViewBookings() {
-//   const [currentPage, setCurrentPage] = useState(1);
-
-//   const { venues, meta, loading, error } = useProfileVenues({ page: currentPage, limit: 100 });
-
-//   console.log('VENUES:', venues);
-
+// function Welcome({ handleViewChange }) {
 //   return (
-//     <section>
-//       <Heading level='2' className='text-center text-custom-coral'>
-//         active bookings
-//       </Heading>
-//     </section>
+//     <div className="flex flex-col gap-10 md:gap-15 mx-5 md:mx-7.5 lg:mx-10 mb-5 md:mb-7.5 lg:mb-10">
+//       <div className="flex flex-col gap-5 md:gap-7.5 md:text-lg lg:text-xl">
+//         <p>
+//           Welcome to the <span className="capitalize font-bold">venue HUB</span>{" "}
+//           — your central dashboard for managing your rental properties with ease
+//           and efficiency!
+//         </p>
+//         <p>
+//           Here in the Venue HUB, you have complete control over your listed
+//           venues and bookings. Whether you&apos;re managing a single cozy
+//           bungalow or a portfolio of stunning properties, Venue HUB provides you
+//           with the tools you need to succeed.
+//         </p>
+//       </div>
+//       <section className="flex flex-col gap-5 md:gap-7.5">
+//         <Heading level="2" className="text-center">
+//           What can you do in the venue HUB?
+//         </Heading>
+//         <Paragraph
+//           spanContent="view venues: "
+//           content="Keep track of all your listed properties in one convenient location. Update details, check availability, and ensure your venues are always presented at their best."
+//         />
+//         <Paragraph
+//           spanContent="view bookings: "
+//           content="Stay organized and never miss a booking! View upcoming reservations, manage inquiries, and maintain clear communication with your guests."
+//         />
+//         <Paragraph
+//           spanContent="create new venue: "
+//           content="Effortlessly add all your venues, wether it be one or many. Add amazing photos, write exciting descriptions and pick your amenities. Holidaze has you covered."
+//         />
+//       </section>
+//       <CreateNewBtn handleViewChange={handleViewChange} />
+//     </div>
 //   );
 // }
-
-function Welcome({ handleViewChange }) {
-  return (
-    <div className="flex flex-col gap-10 md:gap-15 mx-5 md:mx-7.5 lg:mx-10 mb-5 md:mb-7.5 lg:mb-10">
-      <div className="flex flex-col gap-5 md:gap-7.5 md:text-lg lg:text-xl">
-        <p>
-          Welcome to the <span className="capitalize font-bold">venue HUB</span>{" "}
-          — your central dashboard for managing your rental properties with ease
-          and efficiency!
-        </p>
-        <p>
-          Here in the Venue HUB, you have complete control over your listed
-          venues and bookings. Whether you&apos;re managing a single cozy
-          bungalow or a portfolio of stunning properties, Venue HUB provides you
-          with the tools you need to succeed.
-        </p>
-      </div>
-      <section className="flex flex-col gap-5 md:gap-7.5">
-        <Heading level="2" className="text-center">
-          What can you do in the venue HUB?
-        </Heading>
-        <Paragraph
-          spanContent="view venues: "
-          content="Keep track of all your listed properties in one convenient location. Update details, check availability, and ensure your venues are always presented at their best."
-        />
-        <Paragraph
-          spanContent="view bookings: "
-          content="Stay organized and never miss a booking! View upcoming reservations, manage inquiries, and maintain clear communication with your guests."
-        />
-        <Paragraph
-          spanContent="create new venue: "
-          content="Effortlessly add all your venues, wether it be one or many. Add amazing photos, write exciting descriptions and pick your amenities. Holidaze has you covered."
-        />
-      </section>
-      <CreateNewBtn handleViewChange={handleViewChange} />
-    </div>
-  );
-}
 
 // function CreateNewBtn({ handleViewChange }) {
 //   return (
