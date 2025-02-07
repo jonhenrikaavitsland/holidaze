075b9ebe1918576ac51b78a2da333a647a30e5fd
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-02-07 08:05:15 +0100

    creating new venue now includes city

--- src/js/api/useCreateVenue.js
+++ src/js/api/useCreateVenue.js
@@ -23,7 +23,7 @@ const useCreateVenue = (apiUrl, apiKey) => {
         pets,
         venue,
         address,
-        location,
+        chosenLocation,
         zipCode,
         price,
         rating,
@@ -41,7 +41,7 @@ const useCreateVenue = (apiUrl, apiKey) => {
         media9,
       } = values;
 
-      const selectedLocation = locationsMap[location] || {};
+      const selectedLocation = locationsMap[chosenLocation] || {};
       const city = selectedLocation.city || null;
       const lat = selectedLocation.lat || 0;
       const lng = selectedLocation.lng || 0;
