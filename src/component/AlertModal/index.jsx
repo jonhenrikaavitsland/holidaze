8b0bbac16689107a341d89191d43a661e3e603f6
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-02-08 07:43:46 +0100

    buttons stacked on smallest screens

--- src/component/AlertModal/index.jsx
+++ src/component/AlertModal/index.jsx
@@ -1,15 +1,35 @@
 import useAlertStore from "../../js/store/useAlertStore";
+import Heading from "../Heading";
 
 export default function AlertModal() {
-  const { title, message, type, onOk, onCancel } = useAlertStore();
+  const { title, message, type, onOk, onCancel, className } = useAlertStore();
+
   return (
-    <div className="bg-white rounded-xl shadow-md shadow-natural-charcoal/40">
-      <section>{title && <h2>{title}</h2>}</section>
-      {message && <p>{message}</p>}
-      <div className="alert-modal-buttons">
-        <button onClick={onOk}>Ok</button>
-        {type === "ok-cancel" && <button onClick={onCancel}>Cancel</button>}
-      </div>
+    <div className="fixed inset-0 z-50 flex items-center justify-center">
+      <section className="flex flex-col gap-5 bg-white mx-5 p-5 max-w-96 rounded-xl shadow-md shadow-natural-charcoal/40">
+        {title && (
+          <Heading level="2" className="text-center">
+            {title}
+          </Heading>
+        )}
+        {message && <p>{message}</p>}
+        <div className="flex justify-between sm:flex-col flex-wrap gap-5 ">
+          <button
+            className={`${className} py-2 w-24 sm:w-full rounded-xl font-serif uppercase font-medium shadow-md shadow-natural-charcoal/40`}
+            onClick={onOk}
+          >
+            Ok
+          </button>
+          {type === "ok-cancel" && (
+            <button
+              className="bg-deep-blue/20 py-2 w-24 sm:w-full rounded-xl font-serif uppercase font-medium shadow-md shadow-natural-charcoal/40 hover:bg-deep-blue/30"
+              onClick={onCancel}
+            >
+              Cancel
+            </button>
+          )}
+        </div>
+      </section>
     </div>
   );
 }
