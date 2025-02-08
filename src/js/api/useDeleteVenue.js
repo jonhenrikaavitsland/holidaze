37ee5cd1565c617adfb4e94104e12eb44eddd8bd
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-02-08 06:01:53 +0100

    api hook to delete venue

---
+++ src/js/api/useDeleteVenue.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { apiKey, apiUrl, venuesPath } from "../data/constants";
+import useAuthStore from "../store/useAuthStore";
+
+const useDeleteVenue = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const { token } = useAuthStore();
+
+  const deleteVenue = async (id) => {
+    setLoading(true);
+    setError(null);
+
+    try {
+      const response = await fetch(`${apiUrl}${venuesPath}/${id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          "X-Noroff-API-Key": apiKey,
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      // Check if the deletion was successful (204 No Content)
+      if (response.status === 204) {
+        return;
+      } else {
+        throw new Error(
+          `Failed to delete venue. Status code: ${response.status}`,
+        );
+      }
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+  return { deleteVenue, loading, error };
+};
+
+export default useDeleteVenue;
