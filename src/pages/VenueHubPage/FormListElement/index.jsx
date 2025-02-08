79721ee879e3f185ed55e350b710378dd0049f01
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-02-08 14:04:15 +0100

    now using yup

--- src/pages/VenueHubPage/FormListElement/index.jsx
+++ src/pages/VenueHubPage/FormListElement/index.jsx
@@ -44,46 +44,6 @@ export default function FormListElement({
     }
   };
 
-  const getRegisterObj = () => {
-    switch (setter) {
-      case "venue":
-        return { required: true, minLength: 5, maxLength: 40 };
-      case "address":
-        return { required: true, minLength: 8, maxLength: 100 };
-      case "zipCode":
-        return {
-          required: true,
-          minLength: 5,
-          maxLength: 5,
-          min: 35600,
-          max: 35659,
-          RegExp: /^[0-9]+$/,
-        };
-      case "price":
-        return {
-          required: true,
-          minLength: 1,
-          maxLength: 5,
-          min: 1,
-          max: 99999,
-          RegExp: /^[0-9]+$/,
-        };
-      case "sleeps":
-        return {
-          required: true,
-          minLength: 1,
-          maxLength: 2,
-          min: 1,
-          max: 99,
-          RegExp: /^[0-9]+$/,
-        };
-      default:
-        return undefined;
-    }
-  };
-
-  const registerObj = getRegisterObj();
-
   const onChangeHandler = getOnChangeHandler();
 
   return (
@@ -93,23 +53,21 @@ export default function FormListElement({
           {label}
         </label>
         <input
+          {...register(element)}
           className="bg-warm-beige border border-natural-charcoal/40 h-9 text-center font-medium leading-none md:text-lg-leading-none lg:text-xl-leading-none"
           type="text"
           id={element}
           placeholder={placeholder}
           required={mustHave}
-          aria-invalid={!!error}
-          aria-describedby={error ? `${element}-error` : undefined}
           value={value}
           onChange={onChangeHandler}
-          {...register(`${element}`, registerObj)}
         />
         {error && (
           <p
             className="text-custom-coral text-sm-leading-none font-bold text-center"
             id={`${element}-error`}
           >
-            {error}
+            {error?.message}
           </p>
         )}
       </div>
