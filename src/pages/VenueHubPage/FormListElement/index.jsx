99ca3fa19bd162ab50c8c2c41bdf127390fd9392
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-02-08 12:46:59 +0100

    adding react hook form validation

--- src/pages/VenueHubPage/FormListElement/index.jsx
+++ src/pages/VenueHubPage/FormListElement/index.jsx
@@ -1,7 +1,15 @@
 /* eslint-disable react/prop-types */
 import useCreateVenueStore from "../../../js/store/useCreateVenueStore";
 
-export default function FormListElement(props) {
+export default function FormListElement({
+  register,
+  setter,
+  element,
+  placeholder,
+  mustHave,
+  error,
+  label,
+}) {
   const {
     venue,
     address,
@@ -16,11 +24,11 @@ export default function FormListElement(props) {
   } = useCreateVenueStore();
 
   const valueMap = { venue, address, zipCode, price, sleeps };
-  const value = valueMap[props.setter] ?? "";
+  const value = valueMap[setter] ?? "";
 
   // A switch that returns the appropriate setter function
   const getOnChangeHandler = () => {
-    switch (props.setter) {
+    switch (setter) {
       case "venue":
         return (e) => setVenue(e.target.value);
       case "address":
@@ -36,34 +44,72 @@ export default function FormListElement(props) {
     }
   };
 
+  const getRegisterObj = () => {
+    switch (setter) {
+      case "venue":
+        return { required: true, minLength: 5, maxLength: 40 };
+      case "address":
+        return { required: true, minLength: 8, maxLength: 100 };
+      case "zipCode":
+        return {
+          required: true,
+          minLength: 5,
+          maxLength: 5,
+          min: 35600,
+          max: 35659,
+          RegExp: /^[0-9]+$/,
+        };
+      case "price":
+        return {
+          required: true,
+          minLength: 1,
+          maxLength: 5,
+          min: 1,
+          max: 99999,
+          RegExp: /^[0-9]+$/,
+        };
+      case "sleeps":
+        return {
+          required: true,
+          minLength: 1,
+          maxLength: 2,
+          min: 1,
+          max: 99,
+          RegExp: /^[0-9]+$/,
+        };
+      default:
+        return undefined;
+    }
+  };
+
+  const registerObj = getRegisterObj();
+
   const onChangeHandler = getOnChangeHandler();
 
   return (
     <li>
       <div className="flex flex-col gap-1">
-        <label
-          className="text-sm-leading-none capitalize"
-          htmlFor={props.element}
-        >
-          {props.label}
+        <label className="text-sm-leading-none capitalize" htmlFor={element}>
+          {label}
         </label>
         <input
           className="bg-warm-beige border border-natural-charcoal/40 h-9 text-center font-medium leading-none md:text-lg-leading-none lg:text-xl-leading-none"
           type="text"
-          id={props.element}
-          placeholder={props.placeholder}
-          required={props.mustHave}
-          aria-invalid={!!props.error}
-          aria-describedby={props.error ? `${props.element}-error` : undefined}
+          id={element}
+          placeholder={placeholder}
+          required={mustHave}
+          aria-invalid={!!error}
+          aria-describedby={error ? `${element}-error` : undefined}
           value={value}
           onChange={onChangeHandler}
+          {...register(`${element}`, registerObj)}
         />
-        {props.error && (
+        {error && (
           <p
             className="text-custom-coral text-sm-leading-none font-bold text-center"
-            id={`${props.element}-error`}
+            id={`${element}-error`}
           >
-            {props.error}
+            {error}
           </p>
         )}
       </div>
