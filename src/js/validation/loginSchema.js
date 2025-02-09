dafffd26c0f164d364c0eb8a50964086e99f8b39
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-02-09 10:11:09 +0100

    created a login schema for yup

---
+++ src/js/validation/loginSchema.js
@@ -0,0 +1,15 @@
+import * as yup from "yup";
+
+export const schema = yup
+  .object({
+    email: yup
+      .string()
+      .email("Must be a valid email address")
+      .matches(/@stud\.noroff\.no$/, "Email must include @stud.noroff.no")
+      .required("Email is required"),
+    password: yup
+      .string()
+      .min(8, "Password must be at least 8 characters long")
+      .required("Password is required"),
+  })
+  .required();
