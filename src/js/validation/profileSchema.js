15f4fad010acb72508e461fb484f299793af2e30
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-02-09 11:13:03 +0100

    removed the required

--- src/js/validation/profileSchema.js
+++ src/js/validation/profileSchema.js
@@ -2,9 +2,6 @@ import * as yup from "yup";
 
 export const schema = yup
   .object({
-    image: yup
-      .string()
-      .url("Must be a valid and accessible URL.")
-      .required("Please fill in your image URL."),
+    image: yup.string().url("Must be a valid and accessible URL."),
   })
   .required();
