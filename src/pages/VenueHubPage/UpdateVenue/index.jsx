9d9fd70dc5b774b970a93c09ef7941947434d896
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-02-10 10:45:40 +0100

    margin bottom added

--- src/pages/VenueHubPage/UpdateVenue/index.jsx
+++ src/pages/VenueHubPage/UpdateVenue/index.jsx
@@ -149,7 +149,7 @@ export default function UpdateVenue({ venueObj, handleViewChange }) {
       {loading ? (
         <Loader />
       ) : (
-        <div className="flex flex-col gap-10 md:gap-15 lg:gap-20">
+        <div className="flex flex-col gap-10 md:gap-15 lg:gap-20 mb-10 md:mb-15 lg:mb-20">
           <section className="flex flex-col gap-5 md:gap-7.5 lg:gap-10 px-5 md:px-7.5 lg:px-10">
             <Heading level="2" className="text-center text-deep-blue">
               update venue
