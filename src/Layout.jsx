37bf42f7d7093d204ac1a4b38eb469dd617581a7
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-02-10 10:36:05 +0100

    removed margin bottom

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -21,7 +21,7 @@ export default function Layout() {
     <div className="flex flex-col h-screen">
       <Header />
       <main
-        className={`flex-grow lg:container lg:mx-auto mb-10 md:mb-15 lg:mb-20 ${isOverlayOpen ? "overflow-hidden" : ""}`}
+        className={`flex-grow lg:container lg:mx-auto ${isOverlayOpen ? "overflow-hidden" : ""}`}
       >
         <Outlet />
         {isLoginModalOpen && <LoginModal />}
