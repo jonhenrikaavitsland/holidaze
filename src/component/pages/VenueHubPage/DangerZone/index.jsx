f9a9ae90a76e2cd3b2f71c74233261fa75510319
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-02-10 13:59:44 +0100

    moved to new location

--- src/component/pages/VenueHubPage/DangerZone/index.jsx
+++ src/component/pages/VenueHubPage/DangerZone/index.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import DeleteVenueBtn from "../../../component/pages/VenueHubPage/DeleteVenueBtn";
+import DeleteVenueBtn from "../DeleteVenueBtn";
 
 export default function DangerZone({ id, handleViewChange }) {
   return (
