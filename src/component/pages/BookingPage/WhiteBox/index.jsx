48794ccb27c8f5f224adeeb3288eb952d39d26cf
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-02-12 09:05:32 +0100

    added custom focus

--- src/component/pages/BookingPage/WhiteBox/index.jsx
+++ src/component/pages/BookingPage/WhiteBox/index.jsx
@@ -2,7 +2,7 @@
 export default function WhiteBox(props) {
   return (
     <div
-      className={`bg-white w-full border border-natural-charcoal/40 pt-1 px-1 ${!props.isSelect ? "pb-2.5 gap-1" : ""} flex flex-col`}
+      className={`bg-white w-full border border-natural-charcoal/40 pt-1 px-1 ${!props.isSelect ? "pb-2.5 gap-1" : ""} flex flex-col active:ring-transparent focus-within:outline-deep-blue focus-within:outline-2 focus-within:outline`}
     >
       <p className="uppercase text-xs-leading-none font-bold">{props.label}</p>
       <div className="w-full flex justify-center">{props.content}</div>
