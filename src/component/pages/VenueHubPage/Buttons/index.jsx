b21e55426c472c360edc848da650afaeaec01604
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-02-12 10:01:59 +0100

    buttons spaced out on smaller screens

--- src/component/pages/VenueHubPage/Buttons/index.jsx
+++ src/component/pages/VenueHubPage/Buttons/index.jsx
@@ -12,24 +12,24 @@ export default function Buttons({
   const { clearAll } = useCreateVenueStore();
   return (
     <div className="flex w-full mt-5 mb-10 md:mt-7.5 md:mb-15">
-      <ul className="lg:hidden mx-auto text-center bg-warm-beige sm:w-48 w-64 shadow-md shadow-natural-charcoal/40">
+      <ul className="flex flex-col gap-5 md:gap-7.5 lg:hidden mx-auto text-center sm:w-48 w-64 ">
         <LinkBtn
           content="view bookings"
-          className=" hover:font-bold w-full"
+          className="bg-warm-beige border border-natural-charcoal/40 hover:font-bold w-full shadow-md shadow-natural-charcoal/40"
           handleViewChange={handleViewChange}
           kind="booking"
           status={bookingStatus}
         />
         <LinkBtn
           content="view venues"
-          className="border-t border-b border-natural-charcoal/40 w-full hover:font-bold"
+          className="bg-warm-beige border border-natural-charcoal/40 w-full hover:font-bold shadow-md shadow-natural-charcoal/40"
           handleViewChange={handleViewChange}
           kind="venues"
           status={venuesStatus}
         />
         <LinkBtn
           content="new venue"
-          className="w-full hover:font-bold"
+          className="bg-warm-beige w-full border border-natural-charcoal/40 hover:font-bold shadow-md shadow-natural-charcoal/40"
           handleViewChange={handleViewChange}
           kind="newVenue"
           status={newVenueStatus}
