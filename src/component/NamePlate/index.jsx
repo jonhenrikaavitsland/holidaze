ea00f64d483e498a5b3b86e3d7ef48cf9fce0e3e
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-02-13 10:56:24 +0100

    styled to not have name on 2 lines in some cases

--- src/component/NamePlate/index.jsx
+++ src/component/NamePlate/index.jsx
@@ -2,7 +2,7 @@
 export default function NamePlate({ location }) {
   return (
     <div className="flex flex-col items-end justify-end">
-      <div className="bg-custom-coral text-white font-serif font-bold text-lg-leading-none md:text-2xl-leading-none py-4 px-5 md:px-7.5 md:py-5 w-48 md:w-64">
+      <div className="bg-custom-coral text-white font-serif font-bold text-lg-leading-none md:text-2xl-leading-none py-4 px-5 md:px-7.5 md:py-5 text-center w-full">
         <h3>{location.name}</h3>
       </div>
     </div>
