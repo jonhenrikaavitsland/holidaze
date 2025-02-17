f220de1152ad4f3c8df0211c299f1baa7698d756
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-02-17 21:17:01 +0100

    jsdocs nameplate

--- src/component/NamePlate/index.jsx
+++ src/component/NamePlate/index.jsx
@@ -1,4 +1,23 @@
 /* eslint-disable react/prop-types */
+
+/**
+ * Renders a name plate displaying the name of a location.
+ *
+ * This component displays the location name inside a styled container, using a custom background color and typography.
+ * It is typically used as an overlay on a card or image to identify the location.
+ *
+ * @component
+ * @param {object} props - The component props.
+ * @param {object} props.location - The location object.
+ * @param {string} props.location.name - The name of the location to be displayed.
+ *
+ * @example
+ * // Example usage:
+ * <NamePlate location={{ name: "Corralejo" }} />
+ *
+ * @returns {JSX.Element} The rendered name plate component.
+ */
+
 export default function NamePlate({ location }) {
   return (
     <div className="flex flex-col items-end justify-end">
