34f220a5bf0fe2054b303fe67f19a8e7dab3f5a1
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-02-17 21:05:34 +0100

    jsdocs iconsun

--- src/component/IconSun/index.jsx
+++ src/component/IconSun/index.jsx
@@ -1,4 +1,21 @@
 /* eslint-disable react/prop-types */
+
+/**
+ * Renders a sun icon that changes its appearance based on the hover state.
+ *
+ * When the `isHovered` prop is true, the component displays a yellow sun image; otherwise, it displays a white sun image.
+ * This simple component is typically used as a decorative or interactive element in the UI.
+ *
+ * @component
+ * @param {object} props - The component props.
+ * @param {boolean} props.isHovered - Determines which sun image to display based on the hover state.
+ *
+ * @example
+ * // Example usage:
+ * <IconSun isHovered={true} />
+ *
+ * @returns {JSX.Element} The rendered sun icon.
+ */
 export default function IconSun({ isHovered }) {
   return (
     <figure className="p-2.5 md:p-7.5">
