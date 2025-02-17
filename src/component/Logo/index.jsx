21eade60492793b33b55fb02ae145c9ec5b3aa41
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-02-17 21:14:30 +0100

    jsdocs logo

--- src/component/Logo/index.jsx
+++ src/component/Logo/index.jsx
@@ -2,6 +2,28 @@ import { useNavigate } from "react-router-dom";
 import useUIStore from "../../js/store/useUIStore";
 
 /* eslint-disable react/prop-types */
+
+/**
+ * Renders the Holidaze logo as a clickable figure that navigates to the homepage.
+ *
+ * The logo is displayed differently depending on whether it is used inside a modal or not.
+ * When not in a modal and the menu is closed, clicking the logo navigates the user to the root ("/") route.
+ *
+ * @component
+ * @param {object} props - The component props.
+ * @param {boolean} [props.modal] - If true, indicates that the logo is rendered inside a modal, affecting its layout.
+ * @param {string} [props.className] - Optional additional CSS classes to customize the text styling.
+ *
+ * @example
+ * // Example usage:
+ * // In a modal:
+ * <Logo modal={true} className="text-deep-blue" />
+ *
+ * // In a regular header:
+ * <Logo className="text-deep-blue" />
+ *
+ * @returns {JSX.Element} The rendered Logo component.
+ */
 export default function Logo(props) {
   const navigate = useNavigate();
   const { isMenuOpen } = useUIStore();
