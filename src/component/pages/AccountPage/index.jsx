38236f1fa0d790d46aabc90e56ef71dced10566b
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-02-17 17:07:46 +0100

    updating page title and meta description with useEffect

--- src/component/pages/AccountPage/index.jsx
+++ src/component/pages/AccountPage/index.jsx
@@ -4,10 +4,27 @@ import useUserBookings from "../../../js/api/useUserBookings";
 import useAuthStore from "../../../js/store/useAuthStore";
 import BioCard from "./BioCard";
 import BookingCard from "./BookingCard";
+import { useEffect } from "react";
 
 export default function AccountPage() {
   const { user } = useAuthStore();
   const { bookings, loading, error } = useUserBookings();
+
+  useEffect(() => {
+    document.title = "Account || Holidaze";
+
+    const metaDescription = document.querySelector('meta[name="description"]');
+    const content =
+      "Manage your account details and view your upcoming bookings with Holidaze.";
+    if (metaDescription) {
+      metaDescription.setAttribute("content", content);
+    } else {
+      const meta = document.createElement("meta");
+      meta.name = "description";
+      meta.content = content;
+      document.head.appendChild(meta);
+    }
+  }, []);
 
   return (
     <section className="flex flex-col gap-5 md:gap-7.5 lg:gap-10 px-5 md:px-7.5 lg:px-10 pt-5 md:pt-7.5 lg:pt-10 mb-10 md:mb-15 lg:mb-20">
