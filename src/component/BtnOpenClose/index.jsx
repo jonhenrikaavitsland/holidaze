67f6f472191025e9d37646751c26c556491228d6
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-02-17 20:31:50 +0100

    jsdocs btnopenClose

--- src/component/BtnOpenClose/index.jsx
+++ src/component/BtnOpenClose/index.jsx
@@ -1,4 +1,20 @@
 /* eslint-disable react/prop-types */
+
+/**
+ * Renders a button component that visually indicates an open or closed state.
+ *
+ * The component displays a button with an icon that rotates based on the `openState` prop.
+ * When `openState` is true, the icon rotates 180 degrees; otherwise, it remains in its default position.
+ *
+ * @component
+ * @param {object} props - The component props.
+ * @param {boolean} props.openState - Indicates whether the button is in the open state.
+ * @example
+ * // Example usage:
+ * <BtnOpenClose openState={true} />
+ *
+ * @returns {JSX.Element} The rendered button element with a toggling icon.
+ */
 export default function BtnOpenClose({ openState }) {
   return (
     <div className="absolute bottom-0 right-0">
