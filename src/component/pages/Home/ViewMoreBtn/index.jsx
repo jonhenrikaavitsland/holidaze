4e3379b312bc30234e2cf4470ca017953ac070aa
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-02-18 22:45:56 +0100

    jsdocs viewmorebtn

--- src/component/pages/Home/ViewMoreBtn/index.jsx
+++ src/component/pages/Home/ViewMoreBtn/index.jsx
@@ -1,4 +1,22 @@
 /* eslint-disable react/prop-types */
+
+/**
+ * Renders a "load more" button that triggers pagination.
+ *
+ * When clicked, the button calls the provided `paginateData` function with the given `data` and a value of 10,
+ * which typically represents the number of additional items to load.
+ *
+ * @component
+ * @param {object} props - The component props.
+ * @param {*} props.data - The data to paginate.
+ * @param {Function} props.paginateData - The function to execute pagination.
+ *
+ * @example
+ * // Example usage:
+ * <ViewMoreBtn data={data} paginateData={(data, count) => loadMore(data, count)} />
+ *
+ * @returns {JSX.Element} The rendered "load more" button.
+ */
 export default function ViewMoreBtn({ data, paginateData }) {
   return (
     <button
