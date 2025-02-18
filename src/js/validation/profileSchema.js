51e7bda7204915aa1c002aebf85c3ff97c9cdb04
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-02-18 22:20:44 +0100

    jsdocs profileSchema

--- src/js/validation/profileSchema.js
+++ src/js/validation/profileSchema.js
@@ -1,5 +1,14 @@
 import * as yup from "yup";
 
+/**
+ * Validation schema for verifying an image URL.
+ *
+ * This schema expects an object with the following property:
+ * - **image**: A string that must be a valid URL. If the value is not a valid URL, the error message "Must be a valid and accessible URL." is returned.
+ *
+ * @constant {yup.ObjectSchema}
+ * @property {string} image - The URL of the image.
+ */
 export const schema = yup
   .object({
     image: yup.string().url("Must be a valid and accessible URL."),
