4564b1e69cfbb8685dd6bc3b623675dafd86dbc6
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-02-18 22:18:38 +0100

    jsdocs uiStore

--- src/js/store/useUIStore.js
+++ src/js/store/useUIStore.js
@@ -1,5 +1,23 @@
 import { create } from "zustand";
 
+/**
+ * A Zustand store for managing UI state related to modals, menus, and overlays.
+ *
+ * This store maintains boolean flags for various UI components, including:
+ * - `isOverlayOpen`: Whether the overlay is currently open.
+ * - `isMenuOpen`: Whether the mobile menu is open.
+ * - `isLoginModalOpen`: Whether the login modal is open.
+ * - `isRegisterModalOpen`: Whether the register modal is open.
+ * - `isEditProfileOpen`: Whether the edit profile modal is open.
+ * - `isAlertModalOpen`: Whether the alert modal is open.
+ *
+ * The store provides the following actions:
+ * - `openStateWithOverlay(stateKey)`: Opens the overlay and sets the specified state (e.g., `isMenuOpen`) to true.
+ * - `closeAll()`: Closes all UI components by setting all flags to false.
+ * - `checkAndCloseAll()`: Checks if any of the modals or the menu is open and, if so, closes them (except the alert modal).
+ *
+ * @module useUIStore
+ */
 const useUIStore = create((set) => ({
   isOverlayOpen: false,
   isMenuOpen: false,
