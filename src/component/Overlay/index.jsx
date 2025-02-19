23f54098f93a9036c3115c0a7e484658c26cd83e
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-02-19 13:45:26 +0100

    forcing the document to not overflow

--- src/component/Overlay/index.jsx
+++ src/component/Overlay/index.jsx
@@ -7,6 +7,8 @@ import useUIStore from "../../js/store/useUIStore";
  * The overlay dims the background using a semi-transparent dark layer and listens for click events to trigger the `closeAll` function
  * from the UI store, effectively closing any open UI elements. Additionally, if the alert modal is not open, a close button is displayed.
  *
+ * When the overlay is rendered, it forces the user to scroll to the top and disables scrolling on the page until it is closed.
+ *
  * @component
  * @example
  * // Example usage:
@@ -20,11 +22,19 @@ export default function Overlay() {
   // Scroll to the top when the component mounts.
   useEffect(() => {
     window.scrollTo(0, 0);
+    // Disable scrolling on the body.
+    const originalOverflow = document.body.style.overflow;
+    document.body.style.overflow = "hidden";
+
+    // Cleanup function to restore scrolling when the component unmounts.
+    return () => {
+      document.body.style.overflow = originalOverflow;
+    };
   }, []);
 
   return (
     <div
-      className="absolute flex justify-end p-2.5 top-0 left-0 w-dvw h-dvh bg-natural-charcoal/80 z-40 cursor-pointer overflow-hidden"
+      className="absolute flex justify-end p-2.5 top-0 left-0 w-dvw h-dvh bg-natural-charcoal/80 z-40 cursor-pointer"
       onClick={closeAll}
     >
       <div className="lg:container lg:mx-auto flex w-full justify-center md:justify-end ">
